5c0ecea07caeec8eb929be4a586e1a0342091520
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-02 22:20:47 +0530

    set

---
+++ #set=unordered collection of unique elem.py
@@ -0,0 +1,55 @@
+# #set=unordered collection of unique element.
+# #unordered:no index fix
+# #mutable
+# #not allow duolication element 
+
+
+
+# varibale_name={}
+
+
+# student={"anil","riya","john","anil"}
+# print(student)
+
+# numbers={1,2,3,4,5,}
+# print(numbers)
+
+
+# abc=(1,"amit",2.0,"True")
+# print(abc)
+# print(len(abc))
+# print(type(abc))
+# abcd=set(abc)
+# print(abcd)
+
+# student.clear()
+# print(student)
+
+# empty_set=set()
+# print(type(empty_set))
+
+# empty_set={}
+# print(type(empty_set))
+
+# value=()
+# print(type(value)
+      
+student={"amit","sona","mahak"}
+print("sona" in student)
+print(1 not in student)
+
+student.add("jiya")
+print(student)
+set1={1,3,39}
+set2={84,78,94}
+set1.update(set2)
+print(set1)
+
+student={"amit","jiya","mona"}
+student.remove("jiya")
+print(student)
+
+student.discard("mona")
+print(student)
+
+
