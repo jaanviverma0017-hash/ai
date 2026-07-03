dfb04c694f1a6c59b279c51ff70fb60df03fc120
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-03 13:15:16 +0530

    update some concept

--- #set=unordered collection of unique elem.py
+++ #set=unordered collection of unique elem.py
@@ -1,55 +1,69 @@
-# #set=unordered collection of unique element.
-# #unordered:no index fix
-# #mutable
-# #not allow duolication element 
+# # #set=unordered collection of unique element.
+# # #unordered:no index fix
+# # #mutable
+# # #not allow duolication element:if we write so its not print.
+# # set element must be immutable type such as number ,string,or tuple.
+# # stntax
+# # variable={value,value1}
+# # no_value={}#={"
+# # print(type(n0_value))
+# # empty=set()
+# # print(type(empty))  
 
 
 
-# varibale_name={}
 
 
-# student={"anil","riya","john","anil"}
-# print(student)
 
-# numbers={1,2,3,4,5,}
-# print(numbers)
-
-
-# abc=(1,"amit",2.0,"True")
-# print(abc)
-# print(len(abc))
-# print(type(abc))
-# abcd=set(abc)
-# print(abcd)
-
-# student.clear()
-# print(student)
-
-# empty_set=set()
-# print(type(empty_set))
-
-# empty_set={}
+# set()
+# print(type(empty_set)
+# =
+# empty_set={} 
 # print(type(empty_set))
 
 # value=()
 # print(type(value)
       
-student={"amit","sona","mahak"}
-print("sona" in student)
-print(1 not in student)
+# student={"amit","sona","mahak"}
+# print("sona" in student)
+# print(1 not in student)
 
-student.add("jiya")
-print(student)
-set1={1,3,39}
-set2={84,78,94}
-set1.update(set2)
-print(set1)
+# student.add("jiya")
+# print(student)
+# set1={1,3,39}
+# set2={84,78,94}
+# set1.union(set2)
+# print(set1)
 
-student={"amit","jiya","mona"}
-student.remove("jiya")
-print(student)
+# student={"amit","jiya","mona"}
+# student.remove("jiya")
+# print(student)
 
-student.discard("mona")
-print(student)
+# student.discard("mahak")
+# print(student)
+# student.update([2,4,6,8])
+# print(student)
+
+# s={20,40,50,56}
+# s1={86,94,50,78}
+# print(s.union(s1))
+# print(s.intersection(s1)) 
+# print(s.difference(s1)) # remove commen element and print first set
+# print(s.symmetric_difference(s1))#remove comman element 
+# print(s)
+# x={1,2,3,4,5,6,7,8}
+# x.pop() # last element delet 
+# print(x)
+# print(max(x)) #largest value print
+# print(sum(x)) 
+# #iempty_setssubset: the first of the set are in the second set.
+# print(x.issubset(x1))
+# #issuperset:the first set contains all elements all element of the second set.
+# print(x.issuperset(x1))
+# #disjoint:the two set share no element.or all different element will be store
+# print(x.isdisjoint(x1))
 
 
+a=["janviverma@gmail.com","mahisain@gmail.com","priya@gmail.com","janviverma@gmail.com"]
+new_email=set(a)
+print(new_email)
---
+++ dist.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+student={
+  "name":"janvi",
+  "age":20,
+  "village":"bhojawas"
+}
+print(student)
+print(len(student))
+name=dict()
+print(name)
+print(type(name))
+
+pairs=[("a",1),("b",2),("c",3)]
+d=dict(pairs)
+print(d)
+
+mix=[("age",20),("name","jiya"),("number",4),("value",True,)]
+d=dict(mix)
+print(d)
+
+member1=dict(name="janvi",
+             value=12,
+             collage="rps")
+print(member1)            
+
+
+
