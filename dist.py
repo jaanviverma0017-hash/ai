9c4dc652a337eae0fd8a8f87c02c34fd97033830
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-06 12:49:16 +0530

    Update dist.py

--- dist.py
+++ dist.py
@@ -30,5 +30,32 @@ member1=dict(name="janvi",
              collage="rps")
 print(member1)            
 
+#using get (): you can also use get method to access dictionary item.
+#print(student.get("name"))
+#print(student.get("email"))
+#print(student.get("email","email not available"))
+# adding and update dict items
+# adding a new key- value pair
+# student["city"]="delhi"
+# print(student)
+#// updateing an existing value 
+#student["age"]
+#print(student)
 
-
+student={
+    "name":"jaanvi",
+    "age":20,
+    "course":"bca"
+}
+student["age"]
+print(student)
+print(student)
+print(student.pop("age"))
+print(student)
+print(student)
+#pop item
+student.pop()
+print(student)
+##// A dict can contain another dictionary 
+# print(student["student_1"[name]])
+#print(student["student_2"]["course"])
