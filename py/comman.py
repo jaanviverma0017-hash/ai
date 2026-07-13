822b270fd7d3361741648d3c851b5492f6c47e0c
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-13 12:38:03 +0530

    change

---
+++ py/comman.py
@@ -0,0 +1,19 @@
+#)
+student={
+    "student_1":{
+    "name":"janvi",
+    "rollno":20,
+},
+
+    "student_2":{
+    "name":"teena",
+    "rollno":21,
+    }  
+}
+
+# print(student)
+#print(student.get"student_1").get("age", "not found")
+print(student.get["student_1"].get("age","age not found"))
+
+        
+
