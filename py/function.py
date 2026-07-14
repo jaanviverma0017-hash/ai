7f2b6512dae36a409d9bc32e419a6e546665830d
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-14 10:38:42 +0530

    function

---
+++ py/function.py
@@ -0,0 +1,33 @@
+# def print_hello():
+#     print("hello")
+
+# output = print_hello()
+# print(output)   
+
+# def calculate_avg(a, b, c):
+#     sum= a + b + c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+
+# calculate_avg(1, 2, 3)
+
+# def cal_prod(a, b):
+#     print(a * b , a + b)
+#     return a * b , a + b
+
+# cal_prod(4 , 5)
+a=5
+b=5
+def fun(a=4,b):
+    print(a * b , a + b)
+    return a,b 
+fun(b=6)   
+
+# def value(a,b=6):
+#     print(a+b , a*b)
+#     return a,b
+# value(4)
+
+
+
