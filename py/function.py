8cf6a8b5f2182358c010ff33971e718912f5fe7c
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-14 12:02:19 +0530

    code.py

--- py/function.py
+++ py/function.py
@@ -18,16 +18,36 @@
 
 # cal_prod(4 , 5)
 a=5
-b=5
-def fun(a=4,b):
-    print(a * b , a + b)
-    return a,b 
-fun(b=6)   
-
+# b=5
+# # def fun(a=4,b):
+#     # print(a * b , a + b)
+#     return a,b 
+# # fun(b=6)   
+# # 
 # def value(a,b=6):
 #     print(a+b , a*b)
 #     return a,b
 # value(4)
 
+# def message():
+#   return print("python")
+# value = message()
+# print(value)
+
+# def calculate(a,b=2):
+#     a = a + b
+#     b = a * b
+#     return a, b
+# x ,y = calculate(3)
+# print(x+y)
 
 
+def operation(a, b):
+    return a + b
+ 
+def calculate(x):
+    return operation(x, x * 2)
+
+print(calculate(4))
+
+# def calculate(a, b)
