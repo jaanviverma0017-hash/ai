4fad9ce721bdd23f6f80307b05a8215de010063f
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-14 13:35:56 +0530

    fun.py

--- py/function.py
+++ py/function.py
@@ -42,12 +42,64 @@ a=5
 # print(x+y)
 
 
-def operation(a, b):
-    return a + b
+# def operation(a, b):
+#     return a + b
  
-def calculate(x):
-    return operation(x, x * 2)
+# def calculate(x):
+#     return operation(x, x * 2)
 
-print(calculate(4))
+# print(calculate(4))
 
-# def calculate(a, b)
+# sometime we do not know how many argument will be passed to a function.
+# in such cases , we use *args
+# *args stores multiple positional argument  as a tuple
+
+
+#position single star mention
+
+def add_numbers(*numbers):
+    total=0
+    for num in numbers:
+     total += num
+    return total
+print(add_numbers(10, 20))
+print(add_numbers(5, 10, 15, 20))
+
+def show_names(*names):
+   for name in names:
+    print(name)
+show_names("jiya","teena","mahak")      
+      
+def show_name(greet,*name):
+  for name in names:
+   print(greet,hello)
+show_names("jiya","teena","mahak")
+
+#keywords double star mention 
+
+def show_details(**details):
+  for key, value in details.items():
+   print(key,":", value)
+show_details(names="rohit", age=20, city="delhi")
+
+def create_profile(**user):
+  print("user profile")
+  print("name", user.get("name"))
+  print("age",user.get("age"))
+  print("email",user.get("email"))
+create_profile(name="janvi",age=20,email="janviverma@gmail.com")
+
+count=0
+
+def increase():
+  global count
+  count +=1
+increase()
+increase()
+print(count)
+
+
+
+
+
+
