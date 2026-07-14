b43cdc07e56164d138cde599ac21ec3aac4b13d3
jaanvi-0803 <jaanviverma0017@gmail.com>
2026-07-14 23:37:38 +0530

    update.py

--- py/function.py
+++ py/function.py
@@ -165,6 +165,13 @@ def factorial(n):
   else:
     return n * factorial(n - 1)
 print(factorial(5))  
+
+def factorial(n):
+  if n == 0:
+    return 1 
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
   
 
 
